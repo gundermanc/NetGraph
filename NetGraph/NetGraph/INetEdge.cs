7c6e71f04abefb16b70a9491154f5efc513e65c5
Christian Gunderman <gundermanc@gmail.com>
2015-08-24 16:31:32 -0400

    Added missing INetEdge file.

---
+++ NetGraph/NetGraph/INetEdge.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="INetEdge.cs" company="Gundersoft">
+//     Copyright (c) Christian Gunderman. All rights reserved.
+// </copyright>
+// <author>Christian Gunderman</author>
+//-----------------------------------------------------------------------
+
+namespace NetGraph
+{
+    /// <summary>
+    /// The interface for the graph edges between computers.
+    /// </summary>
+    public interface INetEdge
+    {
+        /// <summary>
+        /// The computer that this edge connects to.
+        /// </summary>
+        INetNode Destination { get; }
+
+        /// <summary>
+        /// Indicates that there exists an edge in the opposite direction.
+        /// </summary>
+        bool IsBidirectional { get; }
+
+        /// <summary>
+        /// The computer that this edged emits from.
+        /// </summary>
+        INetNode Origin { get; }
+    }
+}
